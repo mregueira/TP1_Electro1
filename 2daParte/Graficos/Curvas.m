4153e34df3160eec43d21e6ac364f75380f2ead6
Marcelo Regueira <mregueira@itba.edu.ar>
2018-09-17 16:30:13 -0300

    listo parte 2

--- 2daParte/Graficos/Curvas.m
+++ 2daParte/Graficos/Curvas.m
@@ -1,13 +1,13 @@
-semilogx(freq,ratio,'color','red','linewidth',2);
+semilogx(freq,Zout,'color','red','linewidth',2);
 hold on;
-for i=1:301
-    temp = sscanf(Vout{i},'%f,%f');
-    ratio_Sim(i) = 20*log10(sqrt((temp(1)^2)+(temp(2)^2)));
-end
-semilogx(freq_Sim,ratio_Sim,'color','blue','linewidth',2)
+% for i=1:301
+%     temp = sscanf(Vout{i},'%f,%f');
+%     ratio_Sim(i) = 20*log10(sqrt((temp(1))^2 + (temp(2))^2));
+% end
+% semilogx(freq_Sim,ratio_Sim,'color','blue','linewidth',2)
 grid on;
 xlabel('Frecuencia [HZ]');
-ylabel('Avs [dB]');
-legend('Medido','Simulado');
+ylabel('Roa [Ohm]');
+legend('Medido');
 set(gcf,'PaperOrientation','landscape');
-print('CurvasRatio','-dpdf','-fillpage');
+print('CurvaRoa','-dpdf','-fillpage');
